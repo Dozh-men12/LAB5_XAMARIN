52f1d78f1d8e4f8fbea11e039344d0565d965077
Oscar <ohohurtado16@gmail.com>
2023-04-20 19:53:59 -0500

    Primer commit

---
+++ Lab05/Lab05/EditorCode.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab05
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorCode : ContentPage
+    {
+        public EditorCode()
+        {
+            InitializeComponent();
+            StackLayout stack = new StackLayout();
+            var Myeditor = new Editor
+            {
+                Text = "I AM AN EDITOR"
+            };
+            stack.Children.Add(Myeditor);
+            Content = stack;
+        }
+    }
+}
